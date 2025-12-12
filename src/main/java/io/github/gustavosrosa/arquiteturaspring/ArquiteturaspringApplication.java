ef3f7cbcd21819747e2b0398c4e3a82cd1644c5e
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-12 12:29:44 -0300

    FEAT: Usando SpringApplicationBuilder para iniciar config do runner do Spring

--- src/main/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,13 +1,18 @@
 package io.github.gustavosrosa.arquiteturaspring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication // Deve ter a annotation spring boot application
 public class ArquiteturaspringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
+		//SpringApplication.run(ArquiteturaspringApplication.class, args);
+		
+		/** Usado para pre-configurar o runner */
+		
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+		builder.run(args);
 	}
 
 }
