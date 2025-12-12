a599daf83ec2921d8ee3c74bbe25b92213e626bc
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-12 14:54:36 -0300

    FEAT: Obtendo um environment a partir do application.properties

--- src/main/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,9 +1,11 @@
 package io.github.gustavosrosa.arquiteturaspring;
 
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication // Deve ter a annotation spring boot application
 public class ArquiteturaspringApplication {
@@ -16,11 +18,21 @@ public class ArquiteturaspringApplication {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(ArquiteturaspringApplication.class);
 		builder.bannerMode(Mode.OFF); // Remove o banner inicial
 		
+		builder.run(args);
+		
 		// Usado para carregar um bean
 		ConfigurableApplicationContext configurableApplicationContext = builder.context();
-		Object produtoRepository = configurableApplicationContext.getBean("produtoRepository");
+		// Object produtoRepository = configurableApplicationContext.getBean("produtoRepository");
 		
-		builder.run(args);
+		builder.profiles("prod");
+		// builder.properties("");
+		
+		/* Obtendo um environment (de dentro do application.properties) */
+		ConfigurableEnvironment environment = configurableApplicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println(applicationName);
+		
+		
 	}
 
 }
