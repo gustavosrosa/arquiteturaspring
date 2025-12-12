cd3ac04e163153a9cca8520fe1e0d20d6a910aee
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-12 12:03:12 -0300

    FEAT: Add initial structure

---
+++ src/test/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gustavosrosa.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
