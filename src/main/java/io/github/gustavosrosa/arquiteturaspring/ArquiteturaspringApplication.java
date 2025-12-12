741726c81f8bce42927242f7cf14233e14e4a266
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-12 14:49:38 -0300

    FEAT: Exemplo de bean sendo carregado

--- src/main/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/io/github/gustavosrosa/arquiteturaspring/ArquiteturaspringApplication.java
@@ -3,6 +3,7 @@ package io.github.gustavosrosa.arquiteturaspring;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication // Deve ter a annotation spring boot application
 public class ArquiteturaspringApplication {
@@ -14,6 +15,11 @@ public class ArquiteturaspringApplication {
 		
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(ArquiteturaspringApplication.class);
 		builder.bannerMode(Mode.OFF); // Remove o banner inicial
+		
+		// Usado para carregar um bean
+		ConfigurableApplicationContext configurableApplicationContext = builder.context();
+		Object produtoRepository = configurableApplicationContext.getBean("produtoRepository");
+		
 		builder.run(args);
 	}
 
